14895d267434e159454e7e5b6eed3d7e1b09ee77
rvadapally <rvadapally@gmail.com>
2025-10-11 22:33:49 -0500

    Update all environments to use Railway API URL for testing

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000'  // .NET API port
+  apiUrl: 'https://truwit-starter-template-production.up.railway.app'  // Railway deployed .NET API
 };
